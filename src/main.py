70e01dacd3c148ec7ee9f8d394ec88436022ec50
trungpq21 <trungpq21@fpt.com>
2024-08-13 16:25:02 +0700

    Add streaming service and test client

---
+++ src/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
+
+import pytchat
+from chat import stream_chat, stream_mock
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Establish fastapi"}
+
+
+@app.get("/chat/{video_id}")
+async def read_chat(video_id: str):
+    chat = pytchat.create(video_id=video_id)
+    return StreamingResponse(stream_chat(chat), media_type="text/event-stream")
---
+++ src/test_client.py
@@ -0,0 +1,18 @@
+import httpx
+import asyncio
+from chat import stream
+
+
+async def receive_chat_stream(video_id: str):
+    url = f"http://127.0.0.1:8000/chat/{video_id}"
+
+    async with httpx.AsyncClient() as client:
+        async with client.stream("GET", url) as response:
+            async for line in response.aiter_lines():
+                print(line)
+                if line.startswith("data: "):
+                    chat_message = line[len("data: "):]
+                    print(f"Received: {chat_message}")
+
+if __name__ == "__main__":
+    asyncio.run(receive_chat_stream("tGfQYbArQhc"))
