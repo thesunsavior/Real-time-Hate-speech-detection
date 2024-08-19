7eb68c31b579b7c3f2d9ea3c69573d8d590ee210
thesunsavior <20trung.pq@vinuni.edu.vn>
2024-08-19 09:12:25 +0700

    Add queue support for classification service

--- src/classification_service/classification.py
+++ src/classification_service/classification.py
@@ -1,18 +1,27 @@
+import pika
+import pickle
 import pytchat
 
-import pickle
 from fastapi import FastAPI, WebSocket
+
+import json
 
 from model.logistic_regression import LogisticRegression
 from sentence_classification import produce_classification_inference
 from config import Config
 
 config = Config()
+
+# Load up the model
 input_model = pickle.load(open(f"{config.MODEL_FOLDER}/model.pkl", "rb"))
 vectorizer = pickle.load(open(f"{config.MODEL_FOLDER}/vectorizer.pkl", "rb"))
 
 model = LogisticRegression(input_model, vectorizer)
 infer_fn = produce_classification_inference(model)
+
+# Set up queue connection
+connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq"))
+channel = connection.channel()
 
 app = FastAPI()
 
@@ -26,8 +35,23 @@ def read_root():
 async def read_chat(websocket: WebSocket, video_id: str):
     await websocket.accept()
     chat = pytchat.create(video_id=video_id)
+
     while chat.is_alive():
         for c in chat.get().items:
             message = c.message
-            message = infer_fn(message)
-            await websocket.send_text(message)
+            prediction = infer_fn(message)
+            json_message = {"id": c.id, "prediction": prediction}
+
+            # directly send the message to the client
+            await websocket.send_text(json.dumps(json_message))
+
+            # queue the message if prediction is above threshold
+            if prediction > config.THRESHOLD:
+                channel.basic_publish(
+                    exchange="",
+                    routing_key="classification",
+                    body=json.dumps(json_message),
+                    properties=pika.BasicProperties(
+                        delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE
+                    ),
+                )
--- src/classification_service/config.py
+++ src/classification_service/config.py
@@ -4,3 +4,4 @@ import os
 class Config:
     DATA_FOLDER = os.environ.get('DATA_FOLDER') or './data'
     MODEL_FOLDER = os.environ.get('MODEL_FOLDER') or './model'
+    THRESHOLD = float(os.environ.get('THRESHOLD') or 0.5)
